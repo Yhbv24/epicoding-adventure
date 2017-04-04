66e80596eef6cdfc7bf6aa3e9d00774f9987a3f0
Ash Laidlaw and Erica Wright <ash@>
2017-04-04 09:42:22 -0700

    set up firebase and character model

---
+++ src/app/character.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Character } from './character';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
+@Injectable()
+export class CharacterService {
+  characters: FirebaseListObservable<any[]>;
+
+  constructor(private angularFire: AngularFire) {
+    this.characters = angularFire.database.list('characters');
+  }
+
+  getCharacters() {
+    return this.characters;
+  }
+
+  saveCharacter(currentCharacter) {}
+}
--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../character';
+import { CharacterService } from '../character.service';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
-  styleUrls: ['./start.component.css']
+  styleUrls: ['./start.component.css'],
+  providers: [CharacterService]
 })
 export class StartComponent implements OnInit {
+  characters: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private characterService: CharacterService) { }
 
   ngOnInit() {
+    this.characters = this.characterService.getCharacters();
+  }
+
+  startGame(name: string, color: string, track: string, codeTool: string, charSelect: string) {
+    if (charSelect != null) {
+
+    } else {
+      var newCharacter: Character = new Character(name, color, track, codeTool);
+      this.characterService.saveCharacter(newCharacter);
+    }
   }
 
 }
